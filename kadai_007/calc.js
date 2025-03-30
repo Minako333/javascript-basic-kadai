23b2af7e52f1bd7d83e9386fbb798c2b0b8d33cb
Minako333 <mnk11375.c@icloud.com>
2025-03-30 20:27:02 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,16 +1,15 @@
 let num = 45;
 
-// もし変数numが3の倍数ならば、「３の倍数です」という文字列を出力する
-if (num % 3 == 0) {
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
+}
+
+else if (num % 3 == 0) {
   console.log('3の倍数です');
 }
 
 else if (num % 5 == 0) {
   console.log('5の倍数です');
-}
-
-else if (num % 3 == 0 && num % 5 == 0) {
-    console.log('3と5の倍数です');
 }
 
 else {
