1e1efabe74a0b434b3cfc49bb7fd228134a6d334
Minako333 <mnk11375.c@icloud.com>
2025-04-29 16:28:12 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,6 +1,11 @@
-const btn = document.getElementById('');
-const text = document.getElementById('');
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
-btn.addEventListener('', () => {
+// 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+setTimeout(() => {
+  btn.addEventListener('click', () => {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
 
 });
+
