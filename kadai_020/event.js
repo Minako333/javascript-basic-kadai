d9089ac46bc8e8b664052aa518b009c1e0223d46
Minako333 <mnk11375.c@icloud.com>
2025-04-29 16:07:41 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,7 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
 // textというidを持つHTML要素を取得し、定数に代入する
-const btn = document.getElementById('text');
+const textContent = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
