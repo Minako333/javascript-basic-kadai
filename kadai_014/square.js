de3ba04836771d017dde57a18b95019695800b40
Minako333 <mnk11375.c@icloud.com>
2025-04-29 15:31:05 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,7 @@
-const calculateTotal = (number) => {
-    // 与えられた引数numberを二乗し、その値を出力する
-    console.log(number * number);
-  }
+// 与えられた引数numを2倍にし、その値を戻り値として返す関数を定義する
+const calculatateTotal = (num) => {
+  return num * num;
+}
 
-  calculateTotal(10);
-    
+// 関数の戻り値を出力する
+console.log(calculatateTotal(10));
